08aefe84ce47acf35eee773885e0bb7a5e4b2ae5
dima.rgb <dima.rgb@gmail.com>
2015-03-20 20:01:32 +0200

    base makeup

---
+++ scripts/controllers/calendar.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('calendarApp').controller('Calendar', ['$scope', function ($scope) {
+
+    $scope.rows = [1,2,3,4,5,6,7,8,9,0,2,3,4,5,6,7,8,9,0,2,3,4,5,6,7,8,9,0,2,3,4,5,6,7,8,9,0,2,3,4,5,6,7,8,9,0];
+    $scope.days = [1,2,3,4,5,6,7];
+
+}]);
