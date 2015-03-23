12ae167d066f2f56fac883db378d44ed0d5acf09
dima.rgb <dima.rgb@gmail.com>
2015-03-23 12:47:42 +0200

    add unshift and push weeks

--- scripts/controllers/calendar.js
+++ scripts/controllers/calendar.js
@@ -2,7 +2,50 @@
 
 angular.module('calendarApp').controller('Calendar', ['$scope', function ($scope) {
 
-    $scope.rows = [1,2,3,4,5,6,7,8,9,0,2,3,4,5,6,7,8,9,0,2,3,4,5,6,7,8,9,0,2,3,4,5,6,7,8,9,0,2,3,4,5,6,7,8,9,0];
-    $scope.days = [1,2,3,4,5,6,7];
+    var startWeekDay = 1;
+    var msInDay = 24 * 60 * 60 * 1000;
+
+    function getStartWeekDate (date) {
+        var weekDay = date.getDay();
+        var delayWeekDays = (weekDay - startWeekDay + 7) % 7;
+        return new Date(date.getTime() - delayWeekDays * msInDay);
+    }
+
+    function generateWeek (dateTimestamp) {
+        var week = [];
+        var date = new Date(dateTimestamp);
+        var startWeekDate = getStartWeekDate(date);
+        var startWeekTimestamp = startWeekDate.getTime();
+        for( var i = 0; i < 7; i++ ) {
+            week.push(new Date(startWeekTimestamp + i * msInDay));
+        }
+        return week;
+    }
+
+    $scope.now = Date.now();
+    $scope.weeks = [generateWeek($scope.now)];
+
+    $scope.unshiftWeek = function () {
+        var firstWeekDate = $scope.weeks[0][0];
+        var prevWeek = generateWeek(firstWeekDate.getTime() - msInDay);
+        $scope.weeks.unshift(prevWeek);
+        //$scope.weeks.pop();
+    };
+
+    $scope.pushWeek = function () {
+        var lastWeekDate = $scope.weeks[$scope.weeks.length - 1][6];
+        var nextWeek = generateWeek(lastWeekDate.getTime() + msInDay);
+        $scope.weeks.push(nextWeek);
+        //$scope.weeks.shift();
+    };
+
+    $scope.unshiftWeek();
+    $scope.unshiftWeek();
+    $scope.unshiftWeek();
+    $scope.pushWeek();
+    $scope.pushWeek();
+    $scope.pushWeek();
+    $scope.pushWeek();
+
 
 }]);
--- scripts/directives/calendar.js
+++ scripts/directives/calendar.js
@@ -7,31 +7,46 @@ angular.module('calendarApp').directive('calendar', ['$window', '$timeout', func
 
             const weekN = 6;
             var calendarViewport = element[0].getElementsByClassName('calendar__viewport')[0];
-            var calendarTable = element[0].getElementsByClassName('calendar__table')[0];
+            var trHeight = 0;
 
             var lastScrollTop = calendarViewport.scrollTop;
             calendarViewport.onscroll = function (event) {
-                if( lastScrollTop > calendarViewport.scrollTop ) {
+                var scrollTop = calendarViewport.scrollTop;
+                if( lastScrollTop > scrollTop ) {
                     // up
-                    console.log('up');
+                    if( scrollTop < trHeight / 2 ) {
+                        $timeout(function () {
+                            scope.unshiftWeek();
+                            scope.weeks.pop();
+                            calendarViewport.scrollTop += trHeight;
+                        });
+                    }
+                    console.log('up', scrollTop, trHeight);
                 } else {
                     // down
-                    console.log('down');
+                    if( scrollTop > 1.5 * trHeight ) {
+                        $timeout(function () {
+                            scope.pushWeek();
+                            scope.weeks.shift();
+                            calendarViewport.scrollTop -= trHeight;
+                        });
+                    }
+                    console.log('down', scrollTop, trHeight);
                 }
-                lastScrollTop = calendarViewport.scrollTop;
+                lastScrollTop = scrollTop;
             };
-
 
             // on window resize
             $window.onresize = function () {
-                var tableTrs = calendarTable.getElementsByTagName('tr');
-                console.log(calendarViewport.clientHeight);
-                var trHeight = (calendarViewport.clientHeight + weekN / 2) / weekN;
-                for( var i = 0; i < tableTrs.length; i++ ) {
-                    tableTrs[i].style.height = trHeight + 'px';
-                }
+                $timeout(function () {
+                    trHeight = (calendarViewport.clientHeight + weekN / 2) / weekN;
+                    scope.trHeight = trHeight;
+                });
             };
-            $timeout($window.onresize);
+            $window.onresize();
+
+            $timeout(function(){calendarViewport.scrollTop = trHeight;});
+
 
         }
     };
